49dc234ca5af350a8abc3d94fff5396390c07c8e
Paul Moore <p.f.moore@gmail.com>
2014-10-15 21:25:17 +0100

    Added a functional test for generating from a directory

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,7 +1,20 @@
 import subprocess
+from gen.generate import build
 
 def test_basic(tmpdir):
     template = tmpdir / 'template.yaml'
     template.write_text("name: foo\ncontent: bar", encoding='ascii')
     subprocess.check_call(['gen.exe', '--target', str(tmpdir), str(template)])
     assert (tmpdir / 'foo').read() == 'bar'
+
+def test_simple_template(tmpdir):
+    build('test/foo', content='Hello, world', target=str(tmpdir))
+    build('test/bar', content='Hello, bar', target=str(tmpdir))
+    dest = tmpdir.mkdir('target')
+    subprocess.check_call(
+        ['gen.exe', '--target', str(dest), 'test'],
+        cwd=str(tmpdir)
+    )
+    assert len(dest.listdir()) == 2
+    assert (dest / 'foo').read_text() == 'Hello, world'
+    assert (dest / 'bar').read_text() == 'Hello, bar'
