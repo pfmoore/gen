5978ffb193b49e6efdc04884d16fd625e2db54b1
Paul Moore <p.f.moore@gmail.com>
2014-10-15 21:13:54 +0100

    Refactor build_one_file

--- gen/generate.py
+++ gen/generate.py
@@ -17,21 +17,35 @@ def ensuredir(name):
     if not os.path.isdir(name):
         os.mkdir(name)
 
-def build_one_file(defn, cwd=None):
-    filename = defn['name']
-    if cwd:
-        filename = os.path.join(cwd, filename)
+def build(filename, content=None, directory=False, encoding='utf-8', executable=False, target=None):
+    if target:
+        filename = os.path.join(target, filename)
     dirname, basename = os.path.split(filename)
     ensuredir(dirname)
     if not basename:
         return
-    if defn.get('directory'):
+    if directory:
         os.mkdir(filename)
         return
-    with io.open(filename, 'w', encoding=defn.get('encoding', 'utf-8')) as f:
-        f.write(defn.get('content', ''))
-    if defn.get('executable'):
+    with io.open(filename, 'w', encoding=encoding) as f:
+        if content:
+            f.write(content)
+    if executable:
         os.chmod(filename, os.stat(filename).st_mode | stat.S_IXUSR)
+
+# Build a file. Information needed:
+#   - filename
+#   - type (directory or file)
+#   - content (Maybe later - function to generate streamed content?)
+#   - encoding
+#   - executable
+#
+def build_one_file(defn, cwd=None):
+    if 'name' not in defn:
+        raise ValueError("Invalid definition - no filename")
+    filename = defn.pop('name')
+
+    build(filename, target=cwd, **defn)
 
 def build_files(defn, cwd=None):
     for defn in yaml.safe_load_all(defn):
