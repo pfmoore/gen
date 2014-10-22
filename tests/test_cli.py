d6d7a55bc0c12c2ea1d172a669d8ba40ce8ba348
Paul Moore <p.f.moore@gmail.com>
2014-10-22 14:17:45 +0100

    Remove exe suffix in tests so that tests work on Unix

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,7 +4,7 @@ from gen.generate import build
 def test_basic(tmpdir):
     template = tmpdir / 'template.yaml'
     template.write_text("files:\n - name: foo\n   content: bar", encoding='ascii')
-    subprocess.check_call(['gen.exe', '--target', str(tmpdir), str(template)])
+    subprocess.check_call(['gen', '--target', str(tmpdir), str(template)])
     assert (tmpdir / 'foo').read() == 'bar'
 
 def test_simple_template(tmpdir):
@@ -12,7 +12,7 @@ def test_simple_template(tmpdir):
     build('test/bar', content='Hello, bar', target=str(tmpdir))
     dest = tmpdir.mkdir('target')
     subprocess.check_call(
-        ['gen.exe', '--target', str(dest), 'test'],
+        ['gen', '--target', str(dest), 'test'],
         cwd=str(tmpdir)
     )
     assert len(dest.listdir()) == 2
